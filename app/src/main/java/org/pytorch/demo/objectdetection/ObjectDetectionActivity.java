f615681953f7daee20e406d1c7cee9e028ac0686
kykynaris <nnarinratt@gmail.com>
2023-04-03 04:19:49 +0700

    Count Object

--- app/src/main/java/org/pytorch/demo/objectdetection/ObjectDetectionActivity.java
+++ app/src/main/java/org/pytorch/demo/objectdetection/ObjectDetectionActivity.java
@@ -8,6 +8,7 @@ import android.graphics.Matrix;
 import android.graphics.Rect;
 import android.graphics.YuvImage;
 import android.media.Image;
+import android.os.Environment;
 import android.os.Handler;
 import android.util.Log;
 import android.view.TextureView;
@@ -17,6 +18,9 @@ import androidx.annotation.Nullable;
 import androidx.annotation.WorkerThread;
 import androidx.camera.core.ImageProxy;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.pytorch.IValue;
 import org.pytorch.LiteModuleLoader;
 import org.pytorch.Module;
@@ -24,6 +28,8 @@ import org.pytorch.Tensor;
 import org.pytorch.torchvision.TensorImageUtils;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
@@ -67,6 +73,78 @@ public class ObjectDetectionActivity extends AbstractCameraXActivity<ObjectDetec
         mResultView.invalidate();
 
         Exporter.exportObjectToJson(result.mResults, "objectlist.json");
+
+        try {
+            // Get the directory path where the file is stored
+            File directory = Environment.getExternalStorageDirectory();
+            String filePath = directory.getPath() + "/Documents/Vivien/fileJson/objectlist.json";
+
+            // Load the JSON file into a String
+            File file = new File(filePath);
+            if (file.exists()) {
+                FileInputStream inputStream = new FileInputStream(file);
+                int size = inputStream.available();
+                byte[] buffer = new byte[size];
+                inputStream.read(buffer);
+                inputStream.close();
+                String json = new String(buffer, "UTF-8");
+
+                // Parse the JSON file into a JSON array
+                JSONArray jsonArray = new JSONArray(json);
+
+                // Set the classIndex to count
+                int classIndex0 = 0;
+                int classIndex13 = 13;
+                int classIndex56 = 56;
+                int classIndex60 = 60;
+                int classIndex62 = 62;
+                int classIndex72 = 72;
+
+                // Initialize the count variable
+                int count0 = 0;
+                int count1 = 0;
+                int count2 = 0;
+                int count3 = 0;
+                int count4 = 0;
+                int count5 = 0;
+                int count6 = 0;
+
+                // Iterate over the objects in the JSON array
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+                    // Check if the classIndex of the current object is equal to the classIndex to count
+                    if (jsonObject.getInt("classIndex") == classIndex0) {
+                        count0++;
+                    } else if (jsonObject.getInt("classIndex") == classIndex13) {
+                        count1++;
+                    }else if (jsonObject.getInt("classIndex") == classIndex56) {
+                        count2++;
+                    }else if (jsonObject.getInt("classIndex") == classIndex60) {
+                        count3++;
+                    }else if (jsonObject.getInt("classIndex") == classIndex62) {
+                        count4++;
+                    }else if (jsonObject.getInt("classIndex") == classIndex72) {
+                        count5++;
+                    }else {
+                        count6++;
+                    }
+                }
+
+                // Print the count
+                Log.i("JSON Count", "Count of person " + ": " + count0);
+                Log.i("JSON Count", "Count of bench " + ": " + count1);
+                Log.i("JSON Count", "Count of chair " + ": " + count2);
+                Log.i("JSON Count", "Count of table " + ": " + count3);
+                Log.i("JSON Count", "Count of window " + ": " + count4);
+                Log.i("JSON Count", "Count of door "+ ": " + count5);
+                Log.i("JSON Count", "Count of object " + ": " + count6);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private Bitmap imgToBitmap(Image image) {
